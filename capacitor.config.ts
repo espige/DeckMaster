d9d20a906e2953001b042fa9efcdd45be702905c
espige <ellie.spigelman@gmail.com>
2021-09-10 10:29:44 -0400

    initialized code from ionic react sidemenu template

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'DeckMaster',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
