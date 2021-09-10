13cb76ef48a6cb37df12bd01061244300dbdd664
espige <ellie.spigelman@gmail.com>
2021-09-10 11:00:56 -0400

    generated code changes

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'DeckMaster',
+  appName: 'deck-master',
   webDir: 'build',
   bundledWebRuntime: false
 };
