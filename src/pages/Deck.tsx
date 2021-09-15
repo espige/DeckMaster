f682e38de6a93a815059d06d2c8baa424fa0b716
espige <ellie.spigelman@gmail.com>
2021-09-15 09:53:55 -0400

    duplicated options from menu into action sheet, comparing how the two work

--- src/pages/Deck.tsx
+++ src/pages/Deck.tsx
@@ -1,17 +1,24 @@
-import React from 'react';
+import React, { useContext, useState } from 'react';
 import {
+  IonActionSheet,
+  IonButton,
   IonButtons,
   IonContent,
   IonHeader,
+  IonIcon,
   IonMenuButton,
   IonPage,
   IonTitle,
   IonToolbar,
+  NavContext,
 } from '@ionic/react';
 import { useParams } from 'react-router';
+import { addCircleOutline } from 'ionicons/icons';
 
 const Page: React.FC = () => {
   const { name } = useParams<{ name: string }>();
+  const { navigate } = useContext(NavContext);
+  const [actionsOpen, setActionsOpen] = useState(false);
 
   return (
     <IonPage>
@@ -19,6 +26,11 @@ const Page: React.FC = () => {
         <IonToolbar>
           <IonButtons slot="start">
             <IonMenuButton />
+          </IonButtons>
+          <IonButtons slot="end">
+            <IonButton onClick={() => setActionsOpen(true)}>
+              <IonIcon slot="icon-only" icon={addCircleOutline} />
+            </IonButton>
           </IonButtons>
           <IonTitle>{name}</IonTitle>
         </IonToolbar>
@@ -31,6 +43,31 @@ const Page: React.FC = () => {
           </IonToolbar>
         </IonHeader>
         <h1>TEST</h1>
+        <IonActionSheet
+          isOpen={actionsOpen}
+          onDidDismiss={() => setActionsOpen(false)}
+          buttons={[
+            {
+              text: 'Add Card',
+              handler: () => {
+                navigate('/search', 'forward');
+              },
+            },
+            {
+              text: 'Edit Deck',
+            },
+            {
+              text: 'Export',
+            },
+            {
+              text: 'Save',
+            },
+            {
+              text: 'Cancel',
+              role: 'cancel',
+            },
+          ]}
+        />
       </IonContent>
     </IonPage>
   );
