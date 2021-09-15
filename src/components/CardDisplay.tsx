57b9073f07ad92342f15c5b5913d700f0a67d473
espige <ellie.spigelman@gmail.com>
2021-09-15 13:36:54 -0400

    more work on card display

--- src/components/CardDisplay.tsx
+++ src/components/CardDisplay.tsx
@@ -1,6 +1,7 @@
 /* eslint-disable camelcase */
 import {
   IonCard,
+  IonCardContent,
   IonCardHeader,
   IonCardSubtitle,
   IonCardTitle,
@@ -15,13 +16,33 @@ interface CardDisplayProps {
 
 const CardDisplay: React.FC<CardDisplayProps> = ({ card }) => {
   console.log(card);
-  const { name, type_line, image_uris } = card;
+  const {
+    name,
+    type_line,
+    image_uris,
+    mana_cost,
+    oracle_text,
+    power,
+    toughness,
+  } = card;
   return (
     <IonCard>
-      <IonImg src={image_uris?.art_crop} />
+      <IonImg alt={`Art for ${name}`} src={image_uris?.art_crop} />
       <IonCardHeader>
+        <IonCardSubtitle>{mana_cost}</IonCardSubtitle>
         <IonCardTitle>{name}</IonCardTitle>
         <IonCardSubtitle>{type_line}</IonCardSubtitle>
+        <IonCardContent
+          className="ion-no-padding"
+          style={{ whiteSpace: 'pre-line' }}
+        >
+          {oracle_text}
+        </IonCardContent>
+        {power && toughness && (
+          <IonCardContent>
+            {power}/{toughness}
+          </IonCardContent>
+        )}
       </IonCardHeader>
     </IonCard>
   );
